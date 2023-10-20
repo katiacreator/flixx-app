eb8de2280c259817cace3daf020b567714e54623
katiacreator <kcygibson@gmail.com>
2023-10-20 17:07:57 -0400

    added functionality for adding background images to details page

--- js/script.js
+++ js/script.js
@@ -68,7 +68,11 @@ async function displayPopularShows() {
 // display movie details
 async function displayMovieDetails(){
     const movieId = window.location.search.split('=')[1];
+
     const movie = await fetchAPIData(`movie/${movieId}`)
+
+    // overlay for movie bg image
+    displayBackgroundImage('movie', movie.backdrop_path)
 
     const div = document.createElement('div')
     div.innerHTML = `<div class="details-top">
@@ -130,6 +134,28 @@ async function displayMovieDetails(){
 
     document.querySelector('#movie-details').appendChild(div)
 }
+
+// Display Backdrop On Details Pages
+function displayBackgroundImage(type, backgroundPath) {
+    const overlayDiv = document.createElement('div');
+    overlayDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${backgroundPath})`;
+    overlayDiv.style.backgroundSize = 'cover';
+    overlayDiv.style.backgroundPosition = 'center';
+    overlayDiv.style.backgroundRepeat = 'no-repeat';
+    overlayDiv.style.height = '100vh';
+    overlayDiv.style.width = '100vw';
+    overlayDiv.style.position = 'absolute';
+    overlayDiv.style.top = '0';
+    overlayDiv.style.left = '0';
+    overlayDiv.style.zIndex = '-1';
+    overlayDiv.style.opacity = '0.1';
+  
+    if (type === 'movie') {
+      document.querySelector('#movie-details').appendChild(overlayDiv);
+    } else {
+      document.querySelector('#show-details').appendChild(overlayDiv);
+    }
+  }
 
 // Fetch data from tmdb api
 async function fetchAPIData(endpoint) {
